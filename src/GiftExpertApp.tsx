5f81e7555120df8eeb28f033477569f55397c337
EdouardoCornejo <corl.facturacion@gmail.com>
2023-01-23 18:02:06 -0600

    first commit

---
+++ src/GiftExpertApp.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+interface GiftExpertAppProps {
+    
+}
+ 
+export const GiftExpertApp: FC<GiftExpertAppProps> = () => {
+    return ( 
+    <>
+    <h1>GiftExpertApp</h1>
+    </> );
+}
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GiftExpertApp } from './GiftExpertApp'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <GiftExpertApp />
+  </React.StrictMode>,
+)
